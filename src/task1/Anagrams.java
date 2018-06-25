76db18ac9da795f17d1b2ce0f78e48aba7f586be
Alex <hlushko.olexandr@gmail.com>
2018-06-25 14:13:41 +0300

    task1

--- src/task1/Anagrams.java
+++ src/task1/Anagrams.java
@@ -3,7 +3,7 @@ package task1;
 public class Anagrams {
 
 	public static void main(String[] args) throws Exception {
-		String str = "a1bcd efg!h";
+		String str = "a1bcd1 efg!h";
 		System.out.println(reverseString(str));
 	}
 	
@@ -15,14 +15,12 @@ public class Anagrams {
 			return str;
 		}
 		final String[] WORDS = str.split(" ");
-		String revString = "";
-		String temp = "";
+		StringBuffer reversedPhrase = new StringBuffer();
+		
 		for(String word : WORDS) {
-			temp = reverseWord(word);
-			revString = revString + temp + " ";
-			temp = "";
+			reversedPhrase = reversedPhrase.append(reverseWord(word)).append(" ");
 		}
-		return revString.trim();
+		return reversedPhrase.toString().substring(0, reversedPhrase.toString().length() - 1);
 	}
 	
 	private static String reverseWord(String word){
@@ -32,16 +30,20 @@ public class Anagrams {
 		int countFromLeft = 0;
 		
         while (countFromLeft < countFromRight){
-            if (!Character.isLetter(letters[countFromLeft]))
+            if (!Character.isLetter(letters[countFromLeft])) {
             	countFromLeft++;
-            else if(!Character.isLetter(letters[countFromRight]))
+            	}
+            else {
+            	if(!Character.isLetter(letters[countFromRight])) {
             	countFromRight--;
-            else{
+            	}
+            	else{
                 char tmp = letters[countFromLeft];
                 letters[countFromLeft] = letters[countFromRight];
                 letters[countFromRight] = tmp;
                 countFromLeft++;
                 countFromRight--;
+            	}
             }
         }
         return new String(letters);
